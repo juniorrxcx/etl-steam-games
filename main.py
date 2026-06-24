5468328ab265d9df7f4ea4fd95de37b21159a8f9
juniorrxcx <juniorrxcx13@icloud.com>
2026-06-24 18:01:57 -0300

    feat: implementa a transformacao (Transform) removendo dados nulos e criando metrica de aprovacao

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ import pandas as pd
 
 # 1. EXTRAÇÃO (Extract)
 print("Iniciando a extração dos dados da Steam...")
-
-# Lendo o arquivo CSV e transformando em um DataFrame (tabela inteligente do Pandas)
 df_jogos = pd.read_csv('steam_games_raw.csv')
 
-# Exibindo os dados brutos no terminal para conferência
 print("\n--- Dados Brutos (Raw Data) ---")
 print(df_jogos)
+
+# 2. TRANSFORMAÇÃO (Transform)
+print("\nIniciando a limpeza e transformação dos dados...")
+
+# Passo A: O "Filtro de Qualidade"
+# Remove sumariamente qualquer linha que contenha dados vazios (NaN)
+df_limpo = df_jogos.dropna().copy()
+
+# Passo B: Criando inteligência
+# Vamos calcular a porcentagem real de avaliações positivas de cada jogo
+df_limpo['ApprovalRating(%)'] = (df_limpo['PositiveReviews'] / df_limpo['TotalReviews']) * 100
+
+# Passo C: Estética
+# Arredonda o valor da nova coluna para apenas 2 casas decimais
+df_limpo['ApprovalRating(%)'] = df_limpo['ApprovalRating(%)'].round(2)
+
+print("\n--- Dados Transformados (Clean Data) ---")
+print(df_limpo)
