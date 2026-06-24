fe1ecbb98cc91fc3f8f5cb3f20b0a0666d34bf10
juniorrxcx <juniorrxcx13@icloud.com>
2026-06-24 18:21:26 -0300

    feat: implementa o carregamento (Load) exportando os dados para banco SQLite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sqlite3 # Importando o motor de banco de dados nativo do Python
 
 # 1. EXTRAÇÃO (Extract)
 print("Iniciando a extração dos dados da Steam...")
@@ -11,16 +12,28 @@ print(df_jogos)
 print("\nIniciando a limpeza e transformação dos dados...")
 
 # Passo A: O "Filtro de Qualidade"
-# Remove sumariamente qualquer linha que contenha dados vazios (NaN)
 df_limpo = df_jogos.dropna().copy()
 
 # Passo B: Criando inteligência
-# Vamos calcular a porcentagem real de avaliações positivas de cada jogo
 df_limpo['ApprovalRating(%)'] = (df_limpo['PositiveReviews'] / df_limpo['TotalReviews']) * 100
 
 # Passo C: Estética
-# Arredonda o valor da nova coluna para apenas 2 casas decimais
 df_limpo['ApprovalRating(%)'] = df_limpo['ApprovalRating(%)'].round(2)
 
 print("\n--- Dados Transformados (Clean Data) ---")
 print(df_limpo)
+
+# 3. CARREGAMENTO (Load)
+print("\nIniciando o carregamento no Banco de Dados...")
+
+# Cria um arquivo de banco de dados chamado "steam_catalog.db" (ou conecta se já existir)
+conexao = sqlite3.connect('steam_catalog.db')
+
+# Salva a nossa tabela limpa dentro do banco de dados, com o nome "tb_jogos_aprovados"
+# if_exists='replace' garante que se rodarmos o script de novo, ele atualiza a tabela
+df_limpo.to_sql('tb_jogos_aprovados', conexao, if_exists='replace', index=False)
+
+# Fecha a conexão com o banco (boa prática de segurança e performance)
+conexao.close()
+
+print("\nETL Finalizado com sucesso! Dados salvos em 'steam_catalog.db'.")
